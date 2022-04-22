6ea3f0046fb5f28ea28a01b290ef111351070170
underewarrr <underewar@gmail.com>
2022-04-22 12:11:44 -0300

    Dev: Logo includedd at navbar and included assets

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
+import logo from '../../assets/logo.svg'
 import './navbar.css';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='uiux__navbar'>
+      <div className='uiux__navbar-links'>
+        <div className='uiux__navbar-links_logo'>
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
   )
 }
 
