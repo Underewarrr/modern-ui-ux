957da50507f3377f5639f0ca7ea09df6b7f46051
underewarrr <underewar@gmail.com>
2022-04-22 11:49:09 -0300

    Dev: now can use all variable css in all code

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './App'
+import './index.css'
 
 ReactDOM.render(
     <App />, document.getElementById('root')  // Basic create an HTML element and render the App component
