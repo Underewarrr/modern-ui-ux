80c327d6cda4fa0ad8613be6aad779ae2a890760
underewarrr <underewar@gmail.com>
2022-04-23 23:24:27 -0300

    Fix: Wrong divs position

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import './feature.css';
 
-
-const Feature = ( {title, text }) => {  // tittle and text are props
-  return (
-    <div className='uiux__features-container__ feature'>
-      <div className='uiux__features-container__feature-tittle'>
-       <div>
-        <h1>{title}</h1>
-       </div>
-        <div className='uiux__features-container_feature-text'>
-          {text}
-        </div>
-      </div>
+const Feature = ({ title, text }) => (
+  <div className="uiux__features-container__feature">
+    <div className="uiux__features-container__feature-title">
+      <div />
+      <h1>{title}</h1>
     </div>
-  )
-}
+    <div className="uiux__features-container_feature-text">
+      <p>{text}</p>
+    </div>
+  </div>
+);
 
-export default Feature
+export default Feature;
