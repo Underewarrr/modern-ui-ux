e482b471af2eb7cb42a4bd3cc9e6b6c1efeb6aed
underewarrr <underewar@gmail.com>
2022-04-23 23:13:56 -0300

    Dev: FeatureComponents receiving title and text as props

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 import './feature.css';
 
 
-const Feature = () => {
+const Feature = ( {tittle, text }) => {  // tittle and text are props
   return (
-    <div>Feature</div>
+    <div className='uiux__features-container__ feature'>
+      <div className='uiux__features-container__feature-tittle'>
+       <div>
+        <h1>{tittle}</h1>
+       </div>
+        <div className='uiux__features-container_feature-text'>
+          {text}
+        </div>
+      </div>
+    </div>
   )
 }
 
