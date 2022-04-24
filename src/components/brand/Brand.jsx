ef1b8064f1580ddf6cbd346966c1b6802b07ce22
underewarrr <underewar@gmail.com>
2022-04-23 22:26:29 -0300

    Dev: Included Partners in brandSection

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
-import './brand.css';
+import React from 'react'
+import './brand.css'
+import { google, slack, atlassian, dropbox, shopify } from './imports'
 
+
+const Partners = () => (
+  <><div>
+    <img src={google} alt="google" />
+  </div><div>
+      <img src={slack} alt="slack" />
+    </div><div>
+      <img src={atlassian} alt="atlassian" />
+    </div><div>
+      <img src={dropbox} alt="dropbox" />
+    </div><div>
+      <img src={shopify} alt="shopify" />
+    </div></>
+)
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='uiux__brand section_padding'>
+      teste
+     <Partners />
+    </div>
   )
 }
 
