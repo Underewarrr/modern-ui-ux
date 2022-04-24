b7b0e318eb19f9377fbc4c9c42855b7cbe9ebc28
underewarrr <underewar@gmail.com>
2022-04-24 19:21:52 -0300

    Fix: wrong divs closing..

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -28,20 +28,17 @@ const featuresData = [
 
 
 const Features = () => (
-  <div className="uiux__features-container__feature">
-    <div className="uiux__features-container__feature-title">
-      <div />
-      <h1>The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h1>
-      <p>Request Early Access to Get Started</p>
-    </div>
-    <div className="uiux__features-container__feature-text">
-      {
-        featuresData.map((item, index) => (
-          <Feature title={item.title} text={item.text} key={item.title + index} />
-        ))
-      }
-    </div>
+  <div className="uiux__features section__padding" id="features">
+  <div className="uiux__features-heading">
+    <h1 className="gradient__text">The Future is Now and You Just Need to Realize It. Step into Future Today. & Make it Happen.</h1>
+    <p>Request Early Access to Get Started</p>
   </div>
+  <div className="uiux__features-container">
+    {featuresData.map((item, index) => (
+      <Feature title={item.title} text={item.text} key={item.title + index} />
+    ))}
+  </div>
+</div>
 );
 
 export default Features;
